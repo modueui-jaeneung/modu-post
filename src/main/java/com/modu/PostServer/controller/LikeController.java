364e6e7c1eb6594bf215c4ff0c46040625c7bdbd
new-h2 <h2jin.k@gmail.com>
2023-10-09 16:25:45 +0900

    #18 likeController에 requestMapping (/posts)추가

--- src/main/java/com/modu/PostServer/controller/LikeController.java
+++ src/main/java/com/modu/PostServer/controller/LikeController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController("/posts")
 public class LikeController {
 
     private final LikeService likeservice;
